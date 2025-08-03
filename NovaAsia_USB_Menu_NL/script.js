386fed2a1514f050952ad2da654d408583d41e03
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-08-03 07:19:39 +0200

    Delete NovaAsia_USB_Menu_NL directory

--- NovaAsia_USB_Menu_NL/script.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function () {
-    setInterval(() => {
-        window.scrollBy(0, 1);
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            window.scrollTo(0, 0);
-        }
-    }, 40);
-};
