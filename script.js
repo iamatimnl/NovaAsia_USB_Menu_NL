e2fa149cf08d574fe07e02c2d6fa3c282b813c82
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-08-03 08:07:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+<script>
 window.onload = function () {
     setInterval(() => {
-        window.scrollBy(0, 1);
+        window.scrollBy(0, 2); // 每次向下滚动 2 像素
         if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-            window.scrollTo(0, 0);
+            window.scrollTo(0, 0);   // 到底部就回到顶部
         }
-    }, 40);
+    }, 20); // 每 20 毫秒滚动一次，速度较快
 };
+</script>
